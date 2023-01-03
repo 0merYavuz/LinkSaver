7b30ebeaefc42d82b30c18bac0fbe98bd5408a1e
omer <omer@DESKTOP-57HOJ8G>
2023-01-03 19:55:48 +0300

    Add project files.

---
+++ 28.03.2022/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace _28._03._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnKytEkle = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(109, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(586, 30);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtURL_KeyDown);
+            // 
+            // btnKytEkle
+            // 
+            this.btnKytEkle.Location = new System.Drawing.Point(534, 62);
+            this.btnKytEkle.Name = "btnKytEkle";
+            this.btnKytEkle.Size = new System.Drawing.Size(161, 49);
+            this.btnKytEkle.TabIndex = 5;
+            this.btnKytEkle.Text = "Save";
+            this.btnKytEkle.UseVisualStyleBackColor = true;
+            this.btnKytEkle.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(59, 211);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(636, 296);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(534, 155);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(161, 49);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.TabStop = false;
+            this.btnSil.Text = "Delete";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(59, 63);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(161, 48);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "⬇️Double Click to Redirect URL⬇️";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(311, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.linkLabel1.Location = new System.Drawing.Point(515, 114);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(204, 17);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Choose file path for save URLs";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 515);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnKytEkle);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Link Saver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnKytEkle;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ 28.03.2022/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace _28._03._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Saved URLs", 20000);
+            if (File.Exists(dosyayolu))
+            {
+                getData();
+            }
+        }
+
+        string dosyayolu = @"C:\Users\"+Environment.UserName+@"\Documents\SavedURLs.txt";
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtURL.Text.Trim() == "")
+                MessageBox.Show("There is null input. You can't save null data.");
+            else
+            {
+                String[] row = { txtURL.Text };
+                var roww = new ListViewItem(row);
+                listView1.Items.Add(roww);
+                //--------------------------------------------
+                string veri = txtURL.Text;
+                StreamWriter sw = File.AppendText(dosyayolu);
+                sw.WriteLine(veri);
+                sw.Close();
+                txtURL.Text = "";
+                txtURL.Focus();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                int selected = listView1.SelectedItems[0].Index;
+                listView1.Items.RemoveAt(selected);
+                FileStream fs = new FileStream(dosyayolu, FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    sw.WriteLine(listView1.Items[i].Text);
+                }
+                sw.Close();
+                fs.Close();
+            }
+            getData();
+
+        }
+
+
+        private void getData()
+        {
+            listView1.Items.Clear();
+            string row1;
+            StreamReader sr = new StreamReader(dosyayolu);
+            while (true)
+            {
+
+                row1 = sr.ReadLine();
+                if (row1 == null) { break; }
+                string[] row = { row1 };
+                var roww = new ListViewItem(row);
+                listView1.Items.Add(roww);
+
+            }
+            sr.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtURL.Text = "";
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            string selected = listView1.SelectedItems[0].Text;
+            System.Diagnostics.Process.Start(selected);
+        }
+
+
+        private void txtURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSave_Click(this, new EventArgs());
+            }
+        }
+
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                btnDelete_Click(this, new EventArgs());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
